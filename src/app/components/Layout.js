dbe84d086faa4bba1306bfc8dfcec930191b337c
Otgonjargal S <otgoodiesel@gmail.com>
2024-10-10 17:11:04 +0800

    footer style

--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -4,7 +4,23 @@ const Layout = ({ children }) => {
       {children}
       <footer className="footer">
         <img src="/Footer.png" />
-        <div className="bottom-part"></div>
+        <div className="bottom-part">
+          <p className="text-white text-[24px] font-medium mb-4">
+            스트리머와 게임 개발자를 위한 저작권 문제 해결 솔루션
+          </p>
+          <div className="h-[1px] bg-[#888888]"></div>
+          <p className="text-white text-[20px] font-bold mt-8 mb-4">
+            엘에스웨어(주)
+          </p>
+          <ul className="text-[#C4C4C4] text-[20px] font-medium flex flex-wrap gap-12">
+            <li>대표자명 : 김민수</li>
+            <li>
+              서울특별시 금천구 서부샛길 606(가산동 543-1) 대성디폴리스A동 18층
+            </li>
+            <li>대표전화 : 02-6919-0322</li>
+            <li>사업자등록번호 : 113-81-96801</li>
+          </ul>
+        </div>
       </footer>
     </div>
   );
